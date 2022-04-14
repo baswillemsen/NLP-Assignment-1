af2194fe4f0e8446c761b66cb6112c5e317541f9
baswillemsen <bas.willemsen@hotmail.com>
2022-04-14 10:52:26 +0200

    worked on assignment

--- TODO_analyses.py
+++ TODO_analyses.py
@@ -3,8 +3,6 @@
 import spacy
 from collections import Counter
 import pandas as pd
-# from nltk import word_tokenize
-# from nltk.util import ngrams
 
 nlp = spacy.load('en_core_web_sm')
 input = open("sentences.txt", encoding="utf8")
@@ -35,12 +33,12 @@ doc = nlp(lines)
 #
 # print(num_tokens, num_words, num_types, avg_words_per_sentence, avg_words_length)
 
-# # 2. 2.	Word Classes
+# # 2. Word Classes
 # POStoken = []
 # POSpos = []
 # POStags = []
 # for token in doc:
-#     if not token.is_punct:
+#     if not token.is_punct:            # Y/N punctuation?
 #         POStoken.append(token.text)
 #         POSpos.append(token.pos_)
 #         POStags.append(token.tag_)
@@ -49,21 +47,30 @@ doc = nlp(lines)
 # print(df['POStags'].value_counts())
 # print(df['POStags'].value_counts().sum())
 #
+# print(df[df['POStags'] == 'NN']['POStoken'].value_counts())
+# print(df[df['POStags'] == 'NNP']['POStoken'].value_counts())
+# print(df[df['POStags'] == 'IN']['POStoken'].value_counts())
 # print(df[df['POStags'] == 'DT']['POStoken'].value_counts())
+# print(df[df['POStags'] == 'JJ']['POStoken'].value_counts())
+# print(df[df['POStags'] == 'NNS']['POStoken'].value_counts())
+# print(df[df['POStags'] == 'VBD']['POStoken'].value_counts())
+# print(df[df['POStags'] == '_SP']['POStoken'].value_counts())
+# print(df[df['POStags'] == 'VBN']['POStoken'].value_counts())
+# print(df[df['POStags'] == 'RB']['POStoken'].value_counts())
 
-# # 3.	N-grams
-# BIGtoken = []
-# BIGtags = []
-# for i in range(len(doc)-1):
-#     token = [doc[i], doc[i+1]]
-#     if not token.is_punct:
-#         BIGtoken.append(token.text)
-#         BIGtags.append(token.tag_)
-# df2 = pd.DataFrame({"BIGtoken": BIGtoken,"BIGtags": BIGtags})
-#
-# print(df2['BIGtags'])
+# 3. N-grams
+BIGtoken = []
+BIGtags = []
+for i in range(len(doc)-1):
+    token = [doc[i], doc[i+1]]
+    if not token.is_punct:
+        BIGtoken.append(token.text)
+        BIGtags.append(token.tag_)
+df2 = pd.DataFrame({"BIGtoken": BIGtoken,"BIGtags": BIGtags})
 
-# # 4.	Lemmatization
+print(df2['BIGtags'])
+
+# # 4. Lemmatization
 # LEMtoken = []
 # LEMpos = []
 # LEMlem = []
@@ -77,14 +84,15 @@ doc = nlp(lines)
 # print(df3[df3['LEMpos'] == 'VERB']['LEMlem'].value_counts())
 # print(df3[df3['LEMlem'] == 'find'])
 
-# 5.	NER
-NERtoken = []
-NERent = []
-for ent in doc.ents:
-    NERtoken.append(ent.text)
-    NERent.append(ent.label_)
-df5 = pd.DataFrame({"NERtoken": NERtoken,"NERent": NERent})
-
-print(df5.count())
-print(df5['NERent'].unique())
+# # 5. NER
+# NERtoken = []
+# NERent = []
+# for ent in doc.ents:
+#     NERtoken.append(ent.text)
+#     NERent.append(ent.label_)
+# df5 = pd.DataFrame({"NERtoken": NERtoken,"NERent": NERent})
+#
+# print(df5.count())
+# print(df5['NERent'].unique())
+# print(df5.head(10))
 
